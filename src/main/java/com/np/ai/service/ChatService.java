be26c49c4f2db9f9810c715d96860f86a538c1e4
neeleshkurmi <neeleshp212@gmail.com>
2026-05-24 18:59:01 +0530

    created system prompt fro better response

--- src/main/java/com/np/ai/service/ChatService.java
+++ src/main/java/com/np/ai/service/ChatService.java
@@ -1,9 +1,14 @@
 package com.np.ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class ChatService {
@@ -16,7 +21,20 @@ public class ChatService {
     }
 
     public String getLLMResponse(String query){
-        return geminiChatClient.prompt(query).call().content();
+
+        Message systemMessage = SystemPromptTemplate.builder()
+                .template("you are helpful coding assistant. You are expert in coding.")
+                .build().createMessage();
+        Message userMessage = PromptTemplate.builder()
+                .template("What is {techName} ? and tell me about {techExample}")
+                .build().createMessage(Map.of(
+                        "techName", "spring",
+                        "techExample", "spring boot"
+                ));
+
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return geminiChatClient.prompt(prompt).call().content();
     }
 
 }
