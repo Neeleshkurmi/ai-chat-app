c3c7f4759633cd8bf2ca1cdf543dc2094b5c35fa
neeleshkurmi <neeleshp212@gmail.com>
2026-05-25 21:29:19 +0530

    added stateful chat-memory management

--- src/main/java/com/np/ai/service/ChatService.java
+++ src/main/java/com/np/ai/service/ChatService.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
